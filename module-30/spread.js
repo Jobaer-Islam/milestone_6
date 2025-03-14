b1c2340542e58c16d2c118b2d0bb5e9a83c8ac4a
Jobaer Islam <jobaerislam16@gmail.com>
2025-03-15 00:06:42 +0600

    spread operator

---
+++ module-30/spread.js
@@ -0,0 +1,9 @@
+const sahabas = ["Mus'ab", "Udaida", "Umar"];
+
+const newSahabas = ["Usman", "Abu-Bakar", "Hamza", ...sahabas];
+
+console.log(newSahabas.value);
+
+const numbers = [1, 3, 45, 556, 67, 677, 87];
+
+console.log(Math.max(...numbers));
