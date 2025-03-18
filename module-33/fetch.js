d10ae0e0cca653226fd91a869179f003dbbb776f
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-03-19 00:28:19 +0600

    fetch()

---
+++ module-33/fetch.js
@@ -0,0 +1,10 @@
+
+
+const handleFetchData = () => {
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+
+        })
+}
