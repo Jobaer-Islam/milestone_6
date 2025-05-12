2a9aba52841c5eec5c5ff1ec9036f8387253e175
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-12 15:25:31 +0600

    json.stringify and parse()

--- module-36.5(Local Storage)/storage.js
+++ module-36.5(Local Storage)/storage.js
@@ -1,4 +1,23 @@
-const age = 24;
-localStorage.setItem("myage",age);
+// const age = 24;
+// localStorage.setItem("myage",age);
 
-console.log(localStorage.getItem("myage"));
+// console.log(localStorage.getItem("myage"));
+
+const handleaddToStorage = () =>{
+    // const name = "Jobaer Islam";
+    // localStorage.setItem(name,21);
+
+    const person = {
+        name: "hero alom",
+        age: "35",
+        friends:["rohim"]
+    }
+    const convertedToString = JSON.stringify(person);
+
+    localStorage.setItem("alom", convertedToString)
+
+}
+
+const data = localStorage.getItem("alom");
+const convertedToObject = JSON.parse(data);
+console.log(convertedToObject)
