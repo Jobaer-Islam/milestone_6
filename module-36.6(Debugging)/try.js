49b3de00d3c10f5517338c8985ea08bb69ec1c2b
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-14 00:03:49 +0600

    try catch throw

---
+++ module-36.6(Debugging)/try.js
@@ -0,0 +1,27 @@
+const name = "Hajar";
+
+try{
+    // codes
+    console.log("ami cricket khelbo");
+    // console.log(num);
+
+    if(name === "Jobaer"){
+        console.log("Sweet couple")
+    }else{
+        throw "Obviously They are sweet couple"
+    }
+    
+
+}catch(err){
+    // errors
+    console.log(err);
+    // console.log(err.name);
+    // console.log(err.message);
+    // console.error(err)
+    
+
+}
+
+finally{
+    console.log("ami football khelbo");
+}
