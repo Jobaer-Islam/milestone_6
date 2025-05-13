74db71bd278e1b0fc16357c08945e97fc022c6d0
Jobaer Islam <156295576+Jobaer-Islam@users.noreply.github.com>
2025-05-13 20:49:13 +0600

    solve one error

---
+++ module-36.6(Debugging)/error.js
@@ -0,0 +1,28 @@
+// const person = {
+//     name: "hero"
+// }
+
+// console.log(person);
+
+// // console.error("ami");
+
+// console.warn(person)
+
+// console.dir(person)
+
+// console.table(person)
+
+const num1 = 10;
+const num2 = "20";
+console.log(num1, num2);
+
+const res = num1 + num2;
+
+console.log(res);
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+    
+}
